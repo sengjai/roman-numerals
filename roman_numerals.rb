55f4a07e3ae34022829470243fb243df692d30e2
Soon Seng <soonseng.ee@thetechyhub.com>
2016-09-06 08:50:32 +0800

    Trying something different

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,26 +1,43 @@
 def to_roman(num)
   # Your code here
   value = "" #number that we will return
+  letters = ["I","V","X","L","C","D","M"]
+  number = [1,5,10,50,100,500,1000]
+
+
+=begin
+  m = num/number[6]
+  m.times { value << letters[6]}
+  remainder = num % number[6]
+  if remainder >= 900
+  	a << "CM"
+  	remainder = remainder - 900
+  end
+=end
+
+
 
   if num < 0
   	puts 'Please enter a number that isn\'t negative'
   elsif num == 0
   	puts '0'
   else
-  	if num < 10
-  		insertBefore(num)
+  	if num < 5
+  		puts insertBefore(num)
+  	elsif num > 5
+  		puts "V" + insertAfter(num)
+  	else
+  		puts "V"
   	end
   end
 end
 
 def insertBefore(num)
-	if num < 5
-		puts "I"*num; # times the value
-	elsif num > 5
-		puts letter(num) + "I"*(num-5)
-	else
-		puts ""
-	end	
+	return "I"*num; # times the value
+end
+
+def insertAfter(num)
+	return "I"*(num-5); # times the value
 end
 
 def letter(num)
@@ -35,8 +52,7 @@ end
 #puts to_roman(6) == "VI"
 
 
-letters = ["I","V","X","L","C","D","M"]
-number = [1,5,10,50,100,500,1000]
+
 
 
 
